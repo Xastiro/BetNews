cbb962cc41b9a2db26a4c89a41cf184e62c53f10
matthias seeburger <seeburger.matthias@gmail.com>
2022-02-28 17:19:26 +0100

    Actions added to controllers

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,2 +1,20 @@
 class BetsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def confirmation
+  end
+
+  def published
+  end
+
+  def update
+  end
+
+  private
+
 end
