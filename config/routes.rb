749b4cda103e1562ae58ba9595034cb42c280264
matthias seeburger <seeburger.matthias@gmail.com>
2022-02-28 17:54:00 +0100

    Rerouting written better

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,26 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :bets, only: %i[index show] do
+  resources :bets, only: %i[index show new create update] do
     resources :bettings, only: %i[create update]
+    member do
+      get :confirmation
+    end
   end
 
-  get '/bets/:id/confirmation', to: 'bets#confirmation'
+  # get '/bets/:id/confirmation', to: 'bets#confirmation'
 
-  resources :bettings, only: %i[index]
+  resource :profile, only: [] do
+    resources :betting, only: [:index] do
+      collection do
+        get :ongoing
+        get :finished
+        get :published
+      end
+    end
+  end
 
-  get '/profile/bettings/ongoing', to: 'bettings#ongoing'
-  get '/profile/bettings/finished', to: 'bettings#finished'
-  get '/profile/bettings/published', to: 'bets#published'
+  # get '/profile/bettings/ongoing', to: 'bettings#ongoing'
+  # get '/profile/bettings/finished', to: 'bettings#finished'
+  # get '/profile/bettings/published', to: 'bets#published'
 end
