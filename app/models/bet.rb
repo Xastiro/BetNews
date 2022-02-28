6b33be7625875933d1d43d97b09ba73447575037
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-02-28 17:05:49 +0100

    validations models bet

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -4,10 +4,18 @@ class Bet < ApplicationRecord
   has_many :bettings
   belongs_to :users
 
-  validates :hashtag, presence: true
-  validates :description, presence: true
-  validates :question, presence: true
+  validates :hashtag, presence: true, format: { with: /#[\S+]+/, message: "only allows hashtags" }
+  validates :description, presence: true, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
+  validates :question, presence: true, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" }
   validates :photo, presence: true
-  validates :expiring_at, presence: true
   validates :category, presence: true, inclusion: { in: ["Culture", "Economie", "Politique", "Sport", "Planète", "Science", "Technologie", "Santé"]}
+  validates :expiring_at, presence: true
+  validate :expiration_date_cannot_be_in_the_past
+
+  def expiration_date_cannot_be_in_the_past
+    if expiring_at.present? && expiring_at < Date.today
+      errors.add(:expiring_at, "can't be in the past")
+    end
+  end
+
 end
