7a7cf5247580c5e77359c68a18f377207770d381
matthias seeburger <seeburger.matthias@gmail.com>
2022-03-01 11:26:04 +0100

    Add edit route to bet

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :bets, only: %i[index show new create update] do
+  resources :bets, only: %i[index show new create update edit] do
     resources :bettings, only: %i[create update]
     member do
       get :confirmation
