8ec125bb66ff0f586b3335a0bdf5d402fe0b036c
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-03-01 14:24:44 +0100

    premiers seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,11 @@ require "date"
 
 puts 'Cleaning db'
 
-User.destroy_all
+
 Bet.destroy_all
 Betting.destroy_all
 Media.destroy_all
+User.destroy_all
 
 puts 'Creating Users Bets Bettings and Medias'
 
@@ -27,7 +28,7 @@ macron = Bet.new(
   question: "Macron va t'il être réélu ?",
   expiring_at: Date.new(2022,04,22),
   category: "Politique",
-  user: hugo
+  publisher: matthias
 )
 
 zemmour = Bet.new(
@@ -36,7 +37,7 @@ zemmour = Bet.new(
   question: "Eric Zemmour aura t-il ses 500 parrainages ?",
   expiring_at: Date.new(2022,03,04),
   category: "Politique",
-  user: mathis
+  publisher: mathis
 )
 
 ukraine = Bet.new(
@@ -46,16 +47,16 @@ ukraine = Bet.new(
   expiring_at: Date.new(2022,12,31),
   question: "L'Ukraine va t-elle intégrer l'union européenne ?",
   category: "Politique",
-  user: lea
+  publisher: lea
 )
 
 russie = Bet.new(
-  hashtag: "#russie #war #sanction #ukraine",
-  description: "",
-  expiring_at: Date.new(2022,12,31),
-  question: " ?",
+  hashtag: "#petrole #baril",
+  description: "Jusqu'où iront les cours du pétrole et du gaz ? Comme redouté ces derniers jours, le prix du baril de brent a franchi pour la première fois depuis 2014 la barre des 100 dollars. En début de matinée, quand Vladimir Poutine a annoncé le déclenchement de son opération militaire en Ukraine, il a même culminé brièvement à 105 dollars. Même tendance pour le WTI, référence américaine du baril de pétrole, qui se négocie à 96 dollars, après un pic à plus de 100 dollars dans la matinée.",
+  expiring_at: Date.new(2022,04,30),
+  question: " Le prix du baril va t-il dépasser les 120 dollars?",
   category: "Economie",
-  user: gaelle
+  publisher: gaelle
 )
 
 chomage = Bet.new(
@@ -68,7 +69,7 @@ chomage = Bet.new(
   expiring_at: Date.new(2022,03,03),
   question: "Le taux de chomage sera t-il supérieur à 7% dans la zone euro en mars 2022?",
   category: "Economie",
-  user: hugo
+  publisher: hugo
 )
 
 
@@ -78,7 +79,7 @@ cesar = Bet.new(
   expiring_at: Date.new(2022,02,25),
   question: "Pierre Niney va t-il recevoir un César pour le film Boîte noire",
   category: "Culture",
-  user: sarah
+  publisher: sarah
 )
 
 foot = Bet.new(
@@ -87,16 +88,16 @@ foot = Bet.new(
   expiring_at: Date.new(2022,04,17),
   question: "Le PSG va t-il gagner face à l'OM le 24/04?",
   category: "Sport",
-  user: john
+  publisher: john
 )
 
 kohlanta = Bet.new(
   hashtag: "#kohlanta #tvshow",
   description: "Cette nouvelle saison de Koh-Lanta est totalement inédite. C’est du jamais vu : il n’y aura pas un, mais 2 totems cette année ! Celui que vous connaissez, le totem d’immunité, qui protège les gagnants… et le Totem maudit ! Perdre une épreuve, c’est en hériter, et subir l’une de ses nombreuses malédictions. Son ombre plane sur cette saison, du premier au dernier jour de l’aventure. C’est une certitude, le Totem maudit va bousculer les destins des 24 nouveaux aventuriers qui ont décidé de se mesurer les uns aux autres. Dans des conditions dantesques, au pied des spectaculaires falaises de l’archipel de Palawan aux Philippines, ces 24 apprentis Robinsons vont découvrir la vie à la dure, la faim, la fatigue et les stratégies. Avec courage et détermination, ces 12 femmes et ces 12 hommes vont tout faire pour réaliser leur rêve, et gagner Koh-Lanta. Mais la route longue, et semée d’embûches, au premier rang desquelles se trouve, cette année, le Totem maudit.",
-  expiring_at: Date.new(2022,03,08),
+  expiring_at: Date.new(2022,03,10),
   question: "Alexandra va t-elle se faire éliminer de koh lanta mardi prochain?",
   category: "Divertissement",
-  user: christiane
+  publisher: christiane
 )
 
 thevoice = Bet.new(
@@ -105,19 +106,71 @@ thevoice = Bet.new(
   expiring_at: Date.new(2022,05,15),
   question: "Charles va t-il gagner The Voice 2022?",
   category: "Divertissement",
-  user: sarah
+  publisher: sarah
+)
+
+covid = Bet.new(
+  hashtag: "#covid #france",
+  description: "Le ministre de la Santé Olivier Véran a indiqué le 16 février que la fin du port du masque en intérieur était envisageable à la mi-mars si la circulation du Covid-19 est 'très faible'. Un allègement du pass vaccinal est également possible. 'À la mi-mars, si les hôpitaux sont en état normal de fonctionnement, et si la circulation du virus est très faible, nous pourrons enlever les masques pour les adultes et pour les enfants, pour tout ou partie des lieux fermés, y compris sans pass', a déclaré Olivier Véran. 'Nous pourrons commencer à alléger fortement le dispositif du pass vaccinal, peut-être pas tout en bloc, peut-être qu'on gardera des mesures pour la fin mars début avril', a-t-il poursuivi.",
+  expiring_at: Date.new(2022,04,15),
+  question: "Le masque sera t-il toujours obligatoire en France en avril 2022?",
+  category: "Santé",
+  publisher: christiane
+)
+
+chasse = Bet.new(
+  hashtag: "#chasse #france",
+  description: "La chasse à courre, aussi appelée 'vénerie', consiste à traquer un animal (un cerf, sanglier, chevreuil, renard ou lièvre) jusqu’à épuisement à l’aide d’une meute de chiens et de cavaliers à cheval. 84% des Français sont opposés à cette pratique (sondage Ifop 2017) et la chasse à courre est déjà interdite dans plusieurs pays, dont la Grande-Bretagne et l’Allemagne. Très tôt dans la campagne, Yannick Jadot a fait de cette question l’un des marqueurs de sa candidature. Le candidat écologiste propose que les accidents de chasse soient davantage réprimés et que le permis de chasse soit renouvelé tous les cinq ans. Il veut aussi mettre fin à l’agrainage et à la chasse à enclos. Sur cette question, la candidate socialiste Anne Hidalgo ne s’est pas directement prononcée, mais sa porte-parole, Carole Delga, se disait fin octobre sur France Bleu , 'pas d’accord' ​avec cette proposition du candidat écologiste. Jean-Luc Mélenchon rejoint Yannick Jadot sur l’interdiction de la chasse le week-end et lors des vacances scolaires. C’est là que le risque serait le plus grand​, a-t-il déclaré dimanche dernier sur France 3 . Le leader de La France Insoumise propose également de cesser de vendre des armes qui sont aussi puissantes​. Dans son programme, il promet enfin d’interdire les pratiques cruelles telles que le déterrage et la chasse à courre. À droite, tous les candidats s’accordent plus ou moins pour défendre cette pratique. Il faut arrêter la chasse aux chasseurs​, a lancé Valérie Pécresse la semaine dernière lors du grand oral du Mouvement de la ruralité (anciennement ' Chasse, pêche, nature et traditions '​). On n’entrave pas la pêche et la chasse qui sont des libertés issues de la Révolution​, a insisté la candidate LR. Également présent, Eric Zemmour a plaidé pour un grand ministère de l’agriculture de la protection de l’environnement et de la ruralité qui regroupera agriculture, environnement, biodiversité ainsi que la chasse, la pêche et la forêt​.",
+  expiring_at: Date.new(2023,03,15),
+  question: "La chasse à courre sera t-elle interdite en France en 2023 ?",
+  category: "Planète",
+  publisher: gaelle
 )
 
 puts 'Bets well generated'
 
 Betting.create(
-  answer: "yes",
-  user: matthias,
-  bet: first_bet
+  answer: "oui",
+  user: john,
+  bet: macron
+)
+
+Betting.create(
+  answer: "oui",
+  user: mathis,
+  bet: macron
+)
+
+Betting.create(
+  answer: "no",
+  user: gaelle,
+  bet: macron
+)
+
+Betting.create(
+  answer: "non",
+  user: hugo,
+  bet: chomage
+)
+
+Betting.create(
+  answer: "non",
+  user: christiane,
+  bet: foot
+)
+
+Betting.create(
+  answer: "oui",
+  user: hugo,
+  bet: foot
 )
 
 
+Betting.create(
+  answer: "oui",
+  user: sarah,
+  bet: kohlanta
+)
+
 # For photos
 
-file_hibisusdus = URI.open("https://res.cloudinary.com/dbitcpujz/image/upload/v1645527029/baobabs/Trees/hibisusdus.jpg")
-hibisusdus.photo.attach(io: file_hibisusdus, filename: 'nes.jpg', content_type: 'image/jpg')
