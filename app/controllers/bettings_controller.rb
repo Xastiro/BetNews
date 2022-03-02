76fd5e9df2da9a9c4f5a986e9fb667917d70fb5b
matthias seeburger <seeburger.matthias@gmail.com>
2022-03-02 17:19:27 +0100

    Add Smileys images and design for horizontal scroll bar on index

--- app/controllers/bettings_controller.rb
+++ app/controllers/bettings_controller.rb
@@ -1,24 +1,24 @@
 class BettingsController < ApplicationController
 
-  # def create
-  #   @bet = Bet.find(params[:bet_id])
+  def create
+    @bet = Bet.find(params[:bet_id])
 
-  #   normalized_answer = normalized_answer_mapping[params[:commit]]
+    normalized_answer = normalized_answer_mapping[params[:commit]]
 
-  #   @betting = Betting.new(
-  #     answer: normalized_answer,
-  #     bet: @bet,
-  #     user: current_user
-  #   )
+    @betting = Betting.new(
+      answer: normalized_answer,
+      bet: @bet,
+      user: current_user
+    )
 
-  #   if @betting.save!
-  #     flash[:notice] = "Votre pari a bien été pris en compte"
-  #     redirect_to bets_path
-  #   else
-  #     flash[:alert] = "Votre pari n'a pas été pris en compte"
-  #     redirect bet_path(@bet)
-  #   end
-  # end
+    if @betting.save!
+      flash[:notice] = "Votre pari a bien été pris en compte"
+      redirect_to bets_path
+    else
+      flash[:alert] = "Votre pari n'a pas été pris en compte"
+      redirect bet_path(@bet)
+    end
+  end
 
   def yes
     @bet = Bet.find(params[:bet_id])
