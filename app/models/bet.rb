267fa58ed3812b3b0de0a02be4b3501eef0f1a39
matthias seeburger <seeburger.matthias@gmail.com>
2022-03-02 17:41:58 +0100

    Modification of smileys name and refacto Categories

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,6 @@
 class Bet < ApplicationRecord
+  CATEGORIES = ["Culture", "Economie", "Politique", "Sport", "Planète", "Science", "Technologie", "Santé", "Divertissement"]
+
   has_one_attached :photo
   has_many :medias
   has_many :bettings
@@ -9,7 +11,7 @@ class Bet < ApplicationRecord
   validates :description, presence: true, length: { maximum: 2000, message: "2000 characters is the maximum allowed" }
   validates :question, presence: true, length: { maximum: 200, too_long: "%{count} characters is the maximum allowed" }
   validates :photo, presence: true
-  validates :category, presence: true, inclusion: { in: ["Culture", "Economie", "Politique", "Sport", "Planète", "Science", "Technologie", "Santé", "Divertissement"]}
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :expiring_at, presence: true
   # validate :expiration_date_cannot_be_in_the_past
 
