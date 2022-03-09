d66567245c96479f5c0d910a6b1d584776197088
Hugo Ferrand <hugo.ferrand98@audencia.com>
2022-03-09 13:15:09 +0100

    js time out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/workinprogress', to: 'pages#workinprogress'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :bets, only: %i[index show new create edit update] do
