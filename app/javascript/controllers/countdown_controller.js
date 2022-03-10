76efb54ff83e015f54e87f6b04ffbb34f225555b
matthias seeburger <seeburger.matthias@gmail.com>
2022-03-10 18:25:28 +0100

    DateTime fixed with +1 to calculation

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -22,25 +22,25 @@ export default class extends Controller {
 
       const now = new Date().getTime();
 
-      // const date = new Date(this.dateValue).getTime();
       const date = new Date(this.dateValue).getTime();
+      // const date = this.dateValue;
 
       // // Find the distance between now and the count down date
       var distance = date - now;
 
       // // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24) + 1);
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) + 1);
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60) + 1);
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     // console.log(this.dateValue);
     // Output the result in an element with id="countdown"
       if (days > 0) {
-        this.element.innerHTML = "<i class='fas fa-hourglass-half'></i> " + days + 1 + " j";
+        this.element.innerHTML = "<i class='fas fa-hourglass-half'></i> " + days + " j";
       } else if (hours > 0) {
-        this.element.innerHTML = "<i class='fas fa-hourglass-half'></i> " + hours + 1 + " h";
+        this.element.innerHTML = "<i class='fas fa-hourglass-half'></i> " + hours + " h";
       } else if (minutes > 0) {
-        this.element.innerHTML = "<i class='fas fa-hourglass-half'></i> " + minutes + 1 + " min";
+        this.element.innerHTML = "<i class='fas fa-hourglass-half'></i> " + minutes + " min";
       } else if (seconds > 0) {
         this.element.innerHTML = "<i class='fas fa-hourglass-half'></i> " + seconds + " s";
       } else {
